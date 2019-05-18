09fcf4fb850a79f40c5f2c9611c488f0f350f38b
davidramirez7 <davidr.ramirez@gmail.com>
2019-05-18 15:06:12 -0500

    ajustes

--- src/logica/hilo.java
+++ src/logica/hilo.java
@@ -29,10 +29,13 @@ public class hilo extends Thread {
 
     @Override
     public void run() {
-        double t1 = 0, t2 = 0, t3 = 0, ti1 = 0, ti2 = 0, ti3 = 0;
-        float time1 = 0, time2 = 0, time3 = 0;
-        String c1 = "", c2 = "", c3 = "";
+        double t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0, t7 = 0, 
+               ti1 = 0, ti2 = 0, ti3 = 0, ti4 = 0, ti5 = 0, ti6 = 0, ti7 = 0;
+        float time1 = 0, time2 = 0, time3 = 0, time4 = 0, time5 = 0, time6 = 0, time7 = 0;
+        String c1 = "", c2 = "", c3 = "", c4 = "", c5 = "", c6 = "", c7 = "";
         int posicion = 0;
+        
+        
         if (getName().equals("Jarvis")) {
             ti1 = System.currentTimeMillis();
             label.setLocation(0, 0);
@@ -82,8 +85,81 @@ public class hilo extends Thread {
             c3 = (animal + "  -  " + time3 + " segundos\n");
         }//
         
+        //4
+        if (getName().equals("Neron")) {
+            ti4 = System.currentTimeMillis();
+            label.setLocation(0, 0);
+            while (label.getLocation().x < 900) {
+                t4 = System.currentTimeMillis();
+                try {
+                    posicion = (int) (Math.random() * 30 + 1);
+                    label.setLocation(label.getLocation().x + posicion, 170);
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            time4 = (float) ((t4 - ti4) / 1000);
+            c4 = (animal + "  -  " + time4 + " segundos\n");
+        }
+        
+        //5
+        if (getName().equals("Cancerbero")) {
+            ti5 = System.currentTimeMillis();
+            label.setLocation(0, 0);
+            while (label.getLocation().x < 900) {
+                t5 = System.currentTimeMillis();
+                try {
+                    posicion = (int) (Math.random() * 30 + 1);
+                    label.setLocation(label.getLocation().x + posicion, 170);
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            time5 = (float) ((t5 - ti5) / 1000);
+            c5 = (animal + "  -  " + time5 + " segundos\n");
+        }
+        
+        //6
+        if (getName().equals("Guardian")) {
+            ti6 = System.currentTimeMillis();
+            label.setLocation(0, 0);
+            while (label.getLocation().x < 900) {
+                t6 = System.currentTimeMillis();
+                try {
+                    posicion = (int) (Math.random() * 30 + 1);
+                    label.setLocation(label.getLocation().x + posicion, 170);
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            time6 = (float) ((t6 - ti6) / 1000);
+            c6 = (animal + "  -  " + time6 + " segundos\n");
+        }
+        
+        //7
+        if (getName().equals("Estrellita")) {
+            ti7 = System.currentTimeMillis();
+            label.setLocation(0, 0);
+            while (label.getLocation().x < 900) {
+                t7 = System.currentTimeMillis();
+                try {
+                    posicion = (int) (Math.random() * 30 + 1);
+                    label.setLocation(label.getLocation().x + posicion, 170);
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            time3 = (float) ((t7 - ti7) / 1000);
+            c7 = (animal + "  -  " + time7 + " segundos\n");
+        }
+        
+        
         //Retorna las posiciones al panel
-        String posiciones = frmPista.frm.Resultados.getText() + (c1 + c2 + c3);
+        String posiciones = frmPista.frm.Resultados.getText() + (c1 + c2 + c3 + c4 + c5 + c6 + c7);
         frmPista.frm.Resultados.setText(posiciones);
         frmPista.frm.ButInicioC.setEnabled(true);
     }
--- src/vista/frmPista.java
+++ src/vista/frmPista.java
@@ -42,15 +42,19 @@ public class frmPista extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         Resultados = new javax.swing.JTextArea();
         Trueno = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
         Trueno1 = new javax.swing.JLabel();
         Zeus = new javax.swing.JLabel();
         Jarvis = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jSeparator4 = new javax.swing.JSeparator();
-        jSeparator5 = new javax.swing.JSeparator();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
         jSeparator3 = new javax.swing.JSeparator();
+        jSeparator4 = new javax.swing.JSeparator();
+        jSeparator6 = new javax.swing.JSeparator();
+        jSeparator8 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -67,7 +71,7 @@ public class frmPista extends javax.swing.JFrame {
                 ButInicioCActionPerformed(evt);
             }
         });
-        jPanel1.add(ButInicioC, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 420, -1, -1));
+        jPanel1.add(ButInicioC, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 890, -1, -1));
 
         Resultados.setEditable(false);
         Resultados.setColumns(20);
@@ -76,9 +80,8 @@ public class frmPista extends javax.swing.JFrame {
         Resultados.setPreferredSize(new java.awt.Dimension(164, 72));
         jScrollPane1.setViewportView(Resultados);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 380, 260, -1));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 860, 260, -1));
         jPanel1.add(Trueno, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 11, -1, 171));
-        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, 900, 20));
 
         Trueno1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Trueno.gif"))); // NOI18N
         Trueno1.setText("Trueno");
@@ -91,35 +94,60 @@ public class frmPista extends javax.swing.JFrame {
         Jarvis.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Tiro al blanco.gif"))); // NOI18N
         Jarvis.setText("Jarvis");
         jPanel1.add(Jarvis, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 270, -1, 70));
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(988, 601, 59, -1));
-        jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 910, 10));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Trueno - copia (2).gif"))); // NOI18N
+        jLabel3.setText("Neron");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 400, 120, 60));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Zeus - copia 2.gif"))); // NOI18N
+        jLabel4.setText("Cancerbero");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 530, 130, 50));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/blanco2.gif"))); // NOI18N
+        jLabel5.setText("Guardian");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 660, 130, 50));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Trueno - copia(3).gif"))); // NOI18N
+        jLabel6.setText("Estrellita");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 760, 120, 50));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 1530, 10));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 973, -1));
+        jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 1540, 10));
         jSeparator4.getAccessibleContext().setAccessibleParent(jSeparator1);
 
-        jPanel1.add(jSeparator5, new org.netbeans.lib.awtextra.AbsoluteConstraints(916, 107, -1, 75));
+        jPanel1.add(jSeparator6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 372, 1530, 10));
+        jPanel1.add(jSeparator8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, 1540, 20));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Fondo.jpg"))); // NOI18N
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -20, 980, 530));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Fondoracer.jpg"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 0, 1430, 1410));
 
-        getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_END);
-        getContentPane().add(jSeparator3, java.awt.BorderLayout.PAGE_START);
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void ButInicioCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButInicioCActionPerformed
         /**
-         * Se crea 3 hilos para cada caballo en competencia
-         */
+        * Se crea 7 hilos para cada caballo en competencia
+        */
         Resultados.setText("");
         hilo h1 = new hilo("Trueno", jSeparator1.getX(), Trueno1);
         hilo h2 = new hilo("Zeus", jSeparator4.getX(), Zeus);
         hilo h3 = new hilo("Jarvis", jSeparator5.getX(), Jarvis);
+        hilo h4 = new hilo("Nerón", jSeparator5.getX(), Jarvis);
+        hilo h5 = new hilo("Cancerbero", jSeparator5.getX(), Jarvis);
+        hilo h6 = new hilo("Guardián", jSeparator5.getX(), Jarvis);
+        hilo h7 = new hilo("Estrellita", jSeparator5.getX(), Jarvis);
         /**
-         * Arrancan los hilos.
-         */
+        * Arrancan los hilos.
+        */
         h1.start();
         h2.start();
         h3.start();
+        h4.start();
+        h5.start();
+        h6.start();
+        h7.start();
     }//GEN-LAST:event_ButInicioCActionPerformed
 
     /**
@@ -160,19 +188,23 @@ public class frmPista extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JButton ButInicioC;
+    private javax.swing.JButton ButInicioC;
     private javax.swing.JLabel Jarvis;
-    public javax.swing.JTextArea Resultados;
+    private javax.swing.JTextArea Resultados;
     private javax.swing.JLabel Trueno;
     private javax.swing.JLabel Trueno1;
     private javax.swing.JLabel Zeus;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JSeparator jSeparator4;
-    private javax.swing.JSeparator jSeparator5;
+    private javax.swing.JSeparator jSeparator6;
+    private javax.swing.JSeparator jSeparator8;
     // End of variables declaration//GEN-END:variables
 }
